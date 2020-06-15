e50f557183bcfc4652628fa158fa3d616dd056aa
trainee02 <trainee02@ulife.in.th>
2020-06-15 12:47:50 +0700

    add Home

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <Parallex/>
+  </div>
+</template>
+
+<style>
+</style>
+
+<script>
+import Parallex from '../components/Home/Parallex'
+export default {
+  components: {
+    Parallex
+  }
+}
+</script>
